3c1e5adc25d5e0b932cd16e83a17f37042d3afb9
ExonGrunt <jbeard@brainexon.com>
2024-04-19 01:08:48 -0400

    init express.js app

---
+++ index.js
@@ -0,0 +1,42 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3005;
+
+app.use(cors({ origin: true }));
+
+app.get("/", (req, res) => {
+    return res.status(200).send("Hi there, what's up?");
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Save uploaded files to the 'uploads' directory
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname); // Use the original filename for the uploaded file
+    }
+});
+const upload = multer({ storage: storage });
+app.post('/upload', upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded');
+    }
+    res.status(200).send('File uploaded successfully');
+});
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
+
+exports.app = functions.https.onRequest(app);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
